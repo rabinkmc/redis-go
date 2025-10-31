c22b841d20372fe2f2f7eb2da8cbd6188fa773ba
Rabin Dhamala <rabinkmc@gmail.com>
2025-10-31 15:23:58 +0545

    chore: cleanup

--- app/main.go
+++ app/main.go
@@ -6,15 +6,12 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	for {
 		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
+		n, err := conn.Read(buf)
+		fmt.Println("Received data: ", string(buf[:n]))
 		if err != nil {
 			fmt.Println("Error reading from connection: ", err.Error())
 			return
@@ -28,11 +25,6 @@ func handleConnection(conn net.Conn) {
 }
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment the code below to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
