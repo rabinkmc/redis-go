9e97418558300b5f1bf51f7ab67bef7e327e652e
Rabin Dhamala <rabinkmc@gmail.com>
2025-10-31 12:57:27 +0545

    handles multiple request from same connection

--- app/main.go
+++ app/main.go
@@ -21,14 +21,24 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	_, err = conn.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		fmt.Println("Error writing to connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		for {
+			buf := make([]byte, 1024)
+			_, err = conn.Read(buf)
+			if err != nil {
+				fmt.Println("Error reading from connection: ", err.Error())
+				os.Exit(1)
+			}
+			_, err = conn.Write([]byte("+PONG\r\n"))
+			if err != nil {
+				fmt.Println("Error writing to connection: ", err.Error())
+				os.Exit(1)
+			}
+		}
 	}
 }
