81769b88f447baf52aa32e024fd862dcab1fdd01
Rabin Dhamala <rabinkmc@gmail.com>
2025-12-21 15:09:37 +0545

    handle default option

--- app/acl.go
+++ app/acl.go
@@ -6,6 +6,15 @@ func (server *Redis) handleWHOAMI(client *Client, args []string) {
 	client.conn.Write([]byte(resp_bulk_string("default")))
 }
 
+func (server *Redis) handleGETUSER(client *Client, args []string) {
+	// user := args[0]
+	var b strings.Builder
+	b.WriteString("*2\r\n")
+	b.WriteString(resp_bulk_string("flags"))
+	b.WriteString(EMPTY_ARRAY)
+	client.conn.Write([]byte(b.String()))
+}
+
 func is_acl_cmd(cmd string) bool {
 	return cmd == "ACL"
 }
@@ -14,6 +23,8 @@ func (server *Redis) handleACL(client *Client, args []string) {
 	switch cmd {
 	case "WHOAMI":
 		server.handleWHOAMI(client, args[2:])
+	case "GETUSER":
+		server.handleGETUSER(client, args[2:])
 	default:
 		client.conn.Write([]byte(simple_err("shouldn't be here in acl")))
 	}
